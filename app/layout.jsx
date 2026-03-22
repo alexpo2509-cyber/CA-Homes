1f62a12cb16b9d4f5f9dd93497f4a9d3e0177de7
Mr.AP <alexpo2509@gmail.com>
2026-03-22 14:20:25 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Instrument+Serif&display=swap" rel="stylesheet" />
-        <script dangerouslySetInnerHTML={{ __html: `try{const t=localStorage.getItem('ca-theme');if(t)document.documentElement.setAttribute('data-theme',t)}catch(e){}` }} />
       </head>
       <body>{children}</body>
     </html>
